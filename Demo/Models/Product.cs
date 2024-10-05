3e85ea0737b4661382b146e935b9c0c78c1854d9
HuyQuang-hrz <dtc21h4801030084@ictu.edu.vn>
2024-10-05 18:05:05 +0700

    init project

---
+++ Demo/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Demo.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [System.ComponentModel.DisplayName("Mã sản phẩm")]
+        public string? Name { get; set; }
+        [System.ComponentModel.DisplayName("Tên sản phẩm")]
+        public string? Description { get; set; }
+        [System.ComponentModel.DisplayName("Mã thương hiệu")]
+        public string? ImageUrl { get; set; }
+        [System.ComponentModel.DisplayName("Tình trạng")]
+        public decimal Price { get; set; }
+        [System.ComponentModel.DisplayName("Giá")]
+        public int Quantity { get; set; }
+        [System.ComponentModel.DisplayName("Số lượng")]
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public virtual Category? category { get; set; }
+    }
+}
